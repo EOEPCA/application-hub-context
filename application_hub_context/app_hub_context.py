9bf399fdee9e01798748fff2c96264a30aa64760
Fabrice Brito <fabrice.brito@terradue.com>
2023-02-06 13:47:13 +0000

    adds base k8s

--- application_hub_context/app_hub_context.py
+++ application_hub_context/app_hub_context.py
@@ -1,10 +1,25 @@
+import os
 from abc import ABC
+from http import HTTPStatus
+from typing import TextIO
+
+from kubernetes import client, config
+from kubernetes.client import Configuration
+from kubernetes.client.rest import ApiException
+from kubernetes.config.config_exception import ConfigException
 
 
 class ApplicationHubContext(ABC):
-    def __init__(self, namespace, spawner, **kwargs):
+    def __init__(self, namespace, spawner, kubeconfig_file: TextIO = None, **kwargs):
 
+        # k8s
+        self.kubeconfig_file = kubeconfig_file
+        self.api_client = self._get_api_client(self.kubeconfig_file)
+        self.core_v1_api = self._get_core_v1_api()
+        self.batch_v1_api = self._get_batch_v1_api()
+        self.rbac_authorization_v1_api = self._get_rbac_authorization_v1_api()
         self.namespace = namespace
+
         self.spawner = spawner
         # get the groups the user belongs to
         self.user_groups = [group.name for group in self.spawner.user.groups]
@@ -12,8 +27,126 @@ class ApplicationHubContext(ABC):
         self.profile_slug = self.spawner.user_options.get("profile", None)
         # pod env vars
         self.env_vars = {}
+
         # update class dict with kwargs
         self.__dict__.update(kwargs)
+
+    @staticmethod
+    def _get_api_client(kubeconfig_file: TextIO = None):
+
+        try:
+            config.load_incluster_config()  # raises if not in cluster
+            api_client = client.ApiClient()
+            return api_client
+        except ConfigException as exception:
+            print(exception)
+
+        proxy_url = os.getenv("HTTP_PROXY", None)
+        kubeconfig = os.getenv("KUBECONFIG", None)
+
+        if proxy_url:
+            api_config = Configuration(host=proxy_url)
+            api_config.proxy = proxy_url
+            api_client = client.ApiClient(api_config)
+
+        elif kubeconfig:
+            # this is needed because kubernetes-python does not consider
+            # the KUBECONFIG env variable
+            config.load_kube_config(config_file=kubeconfig)
+            api_client = client.ApiClient()
+        elif kubeconfig_file:
+            config.load_kube_config(config_file=kubeconfig)
+            api_client = client.ApiClient()
+        else:
+            # if nothing is specified, kubernetes-python will use the file
+            # in ~/.kube/config
+            config.load_kube_config()
+            api_client = client.ApiClient()
+
+        return api_client
+
+    def _get_core_v1_api(self) -> client.CoreV1Api:
+
+        return client.CoreV1Api(api_client=self.api_client)
+
+    def _get_batch_v1_api(self) -> client.BatchV1Api:
+
+        return client.BatchV1Api(api_client=self.api_client)
+
+    def _get_rbac_authorization_v1_api(self) -> client.RbacAuthorizationApi:
+
+        return client.RbacAuthorizationV1Api(self.api_client)
+
+    def is_object_created(self, read_method, **kwargs):
+
+        read_methods = {}
+
+        read_methods["read_namespace"] = self.core_v1_api.read_namespace
+        read_methods[
+            "read_namespaced_role"
+        ] = self.rbac_authorization_v1_api.read_namespaced_role  # noqa: E501
+        read_methods[
+            "read_namespaced_role_binding"
+        ] = self.rbac_authorization_v1_api.read_namespaced_role_binding  # noqa: E501
+
+        read_methods[
+            "read_namespaced_config_map"
+        ] = self.core_v1_api.read_namespaced_config_map  # noqa: E501
+
+        read_methods[
+            "read_namespaced_persistent_volume_claim"
+        ] = self.core_v1_api.read_namespaced_persistent_volume_claim  # noqa: E501
+
+        read_methods[
+            "read_namespaced_secret"
+        ] = self.core_v1_api.read_namespaced_secret  # noqa: E501
+        try:
+            if read_method in [
+                "read_namespaced_config_map",
+                "read_namespaced_role",
+                "read_namespaced_role_binding",
+                "read_namespaced_persistent_volume_claim",
+                "read_namespaced_secret",
+            ]:
+                read_methods[read_method](namespace=self.namespace, **kwargs)
+            else:
+                read_methods[read_method](self.namespace)
+        except ApiException as exc:
+            if exc.status == HTTPStatus.NOT_FOUND:
+                return None
+            else:
+                raise exc
+        return read_methods
+
+    def is_namespace_created(self, **kwargs):
+
+        return self.is_object_created("read_namespace", **kwargs)
+
+    def is_namespace_deleted(self, **kwargs):
+        """Helper function for retry in dispose"""
+        return not self.is_namespace_created()
+
+    def is_role_binding_created(self, **kwargs):
+
+        return self.is_object_created("read_namespaced_role_binding", **kwargs)
+
+    def is_role_created(self, **kwargs):
+
+        return self.is_object_created("read_namespaced_role", **kwargs)
+
+    def is_config_map_created(self, **kwargs):
+
+        return self.is_object_created("read_namespaced_config_map", **kwargs)
+
+    def is_pvc_created(self, **kwargs):
+
+        return self.is_object_created(
+            "read_namespaced_persistent_volume_claim", **kwargs
+        )  # noqa: E501
+
+    def is_image_pull_secret_created(self, **kwargs):
+
+        return self.is_object_created("read_namespaced_secret", **kwargs)
 
     def get_profile_list(self):
         pass
---
+++ tests/tests_k8s.py
@@ -0,0 +1,75 @@
+import os
+import unittest
+
+from addict import Dict
+from kubernetes import client
+
+from application_hub_context.app_hub_context import DefaulfApplicationHubContext
+
+group = Dict()
+group.name = "group_a"
+groups = [group]
+spawner = Dict()
+
+spawner.user_options = {"profile": "selected_slug"}
+spawner.user.name = "alice"
+spawner.user.groups = groups
+spawner.environment = {}
+
+spawner.profile_list = [
+    {
+        "display_name": "Studio Labs version 0.4",
+        "slug": "ellip_studio_notebook",
+        "default": False,
+        "kubespawner_override": {
+            "cpu_limit": 1,
+            "mem_limit": "8G",
+            "image": "cr.terradue.com/ellip-studio/studio-lab:0.1",
+        },
+    },
+    {
+        "display_name": "Studio Coder version 0.8",
+        "slug": "ellip_studio_coder_slug",
+        "default": False,
+        "kubespawner_override": {
+            "cpu_limit": 1,
+            "mem_limit": "8G",
+            "image": "cr.terradue.com/ellip-studio/studio-coder:0.8",
+        },
+    },
+]
+
+
+class TestK8s(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+
+        os.environ["KUBECONFIG"] = "/home/mambauser/.kube/kubeconfig-t2-dev.yaml"
+
+        cls.app_hub_context = DefaulfApplicationHubContext(
+            namespace="a_namespace",
+            spawner=spawner,
+            a=1,
+            b=2,
+        )
+
+    def test_obj(self):
+
+        self.assertIs(type(self.app_hub_context), DefaulfApplicationHubContext)
+
+    def test_client(self):
+
+        self.assertIsInstance(self.app_hub_context._get_core_v1_api(), client.CoreV1Api)
+
+    def test_batch(self):
+
+        self.assertIsInstance(
+            self.app_hub_context._get_batch_v1_api(), client.BatchV1Api
+        )
+
+    def test_rbac(self):
+
+        self.assertIsInstance(
+            self.app_hub_context._get_rbac_authorization_v1_api(),
+            client.RbacAuthorizationApi,
+        )
